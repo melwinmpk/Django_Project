26893d96340f72f7575a7bc08d5c809e01ccf34f
Melwinmpk <melwinmpk13@gmail.com>
2020-02-28 08:27:49 +0530

    foreignkey constrains

--- testsetup/admin.py
+++ testsetup/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import QuestionDefinition,SubjectDefinition
+from .models import QuestionDefinition,SubjectDefinition,Questions
 # Register your models here.
 
 admin.site.register(QuestionDefinition)
 admin.site.register(SubjectDefinition)
+admin.site.register(Questions)
--- testsetup/models.py
+++ testsetup/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Questions(models.Model):
-    Subjectid  = models.CharField(max_length=100,default=1)
-    Question   = models.CharField(max_length=320,blank=True, null=True)
-    Options    = models.TextField(blank=True, null=True)
-    QuesType   = models.IntegerField(blank=False, null=False,default=1)
-    Ans        = models.IntegerField(blank=False, null=False)
-
-
 
 class QuestionDefinition(models.Model):
-    # QuestionTypeId = models.IntegerField(blank=False, null=False,unique=True)
+    QuestionTypeId = models.AutoField(auto_created = True,primary_key = True,serialize = False,null=False,default=None)
     QuestionType   = models.CharField(max_length=100,blank=True, null=True)
 
 class SubjectDefinition(models.Model):
     SubjectName = models.CharField(max_length=100,blank=True, null=True)
-    # SubjectId   = models.IntegerField(blank=False, null=False,unique=True)
+    SubjectId   = models.AutoField(auto_created = True,primary_key = True,serialize = False,null=False,default=None)
 
+class Questions(models.Model):
+    # Subjectid  = models.CharField(max_length=100,default=1)
+    # QuesType   = models.IntegerField(blank=False, null=False,default=1)
+    SubjectId        = models.ForeignKey(SubjectDefinition,on_delete=models.CASCADE,default=None)
+    Question         = models.CharField(max_length=320,blank=True, null=True)
+    Options          = models.TextField(blank=True, null=True)
+    QuestionTypeId   = models.ForeignKey(QuestionDefinition,on_delete=models.CASCADE,default=None)
+    Ans              = models.IntegerField(blank=False, null=False)
